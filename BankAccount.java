a21aaf9023cd60a9ff5304d7c8a4b5afd3b3546c
Xano Sweeting <xano.sweeting@gmail.com>
2019-10-02 21:28:31 -0400

    Initial commit

---
+++ BankAccount.java
@@ -0,0 +1,21 @@
+public class BankAccount{
+	private String name;
+	private double balance;
+	private double interest;
+
+	BankAccount(){
+		name = "Unkown";
+		balance = 0.0
+		interest = .001
+	}
+	BankAccount(n,b){
+		name = n;
+		balance = b;
+		interest = .001;
+	}
+	BankAccount(n,b,i){
+		name = n;
+		balance = b;
+		interest = i;
+	}
+}
