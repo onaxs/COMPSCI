85a660c0cd42790143c18708d7984a5d9d49f65f
Xano Sweeting <22806082+onaxs@users.noreply.github.com>
2019-10-10 11:34:59 -0400

    Update Die.java

--- Die.java
+++ Die.java
@@ -13,11 +13,15 @@ public class Die{
 		getFaceNum();	
 	}
 
-	public int getFaceNum(){
+	public int getFaceValue(){
 	faceValue = Math.random()*numOfFaces + 1;
 	return faceValue;
 	}
-
+	
+	public int getNumOfFaces(){
+	    return numOfFaces;
+	}
+	
 	public int roll(){
 		faceValue = Math.random()*numOfFaces + 1;
 		return faceValue;
@@ -27,10 +31,8 @@ public class Die{
 		return "Your die has " + this.numOfFaces + " number of faces and the top value is " + this.faceValue;
 	}
 
-	public boolean equals(o1,o2){
-		if o1 == o2
-			return true;
-		else return false;
+	public boolean equals(Die other){
+		return faceValue == other.getFaceValue() && numOfFaces == other.getNumOfFaces();
 	}
 
 
