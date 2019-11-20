1bb894f63980167a97bfa3f8652095dd37bed3fe
Xano Sweeting <xano.sweeting@gmail.com>
2019-11-20 16:54:13 +0000

    chatbot

--- Chat Bot/Bot.java
+++ Chat Bot/Bot.java
@@ -7,4 +7,32 @@ public class ChatBot{
 	public String hobby;
 	public String username;
 
+  public ChatBot(String n,String l){
+    name = n;
+    location = l;
+  }
+
+
+  public String greeting(){
+    return("Hi there, what's your name");
+  }
+
+  public String getUsername(){
+    return username;
+  }
+  
+  public void setUsername(n){
+    username = n;
+  }
+
+  public void setAge(a){
+    age = a;
+  }
+
+
+
+
+
+
+
 }
