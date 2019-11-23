dac65365234016738f5cc8528c7a5b13af4656a4
Xano Sweeting <xano.sweeting@gmail.com>
2019-11-23 17:08:10 -0500

    update

--- Chat Bot/ChatBot.java
+++ Chat Bot/ChatBot.java
@@ -1,4 +1,4 @@
-public class ChatBot{
+spublic class ChatBot{
 
 	public int age;
 	public String name = "Dobby";
@@ -31,6 +31,10 @@ public class ChatBot{
     return("How may I be of assitance master?");
   }
 
+  public String nameGreeting(String phrase){
+    return "Hi "+getUsername()+" my name is Dobby"
+  }
+
   public String goodBye(String phrase){
     return "farewell master " + getUsername();
   }
@@ -49,16 +53,39 @@ public class ChatBot{
     if(phrase.contains("name")||(phrase.contains("call")&&phrase.contains("you"))){
       return "My name is Dobby the elf";
     }
+    else if(phrase.contains(" plus ")||phrase.contains("+")||phrase.contains("minus")||phrase.contains("-")||phrase.contains("divided by")||phrase.contains("/"){
+    }
   }
 
   public String whereResponse(String phrase){
-    if(phrase.contains("you")&&phrase.contains("do")){
+    if(phrase.contains("you")){
       if(phrase.contains("go")){
         return "I return to ... the cloud";
       }
       else if(phrase.contains("live")){
         return "I live in ... the cloud";
       }
+      else if(phrase.contains("from")){
+        return "I am from Hogwarts";
+      }
+      else if(phrase.contains("are")){
+        return "I am currently in the cloud above you";
+      }
+    }
+  }
+
+  public String mathResponse(String phrase){ 
+    if(phrase.contains("plus")||phrase.contains("+"){
+
+    }
+    else if(phrase.contains("minus")||phrase.contains("-")){
+
+    }
+    else if(phrase.contains("times")||phrase.contains("multiplied by")||phrase.contains(" x ")||phrase.contains(" x ")){
+
+    }
+    else if(phrase.contains("divided by")||phrase.contains("/")){
+
     }
   }
 
