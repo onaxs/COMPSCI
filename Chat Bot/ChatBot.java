5f86d2a3a6efe0cd74b56a47d445540fa31fef60
Xano Sweeting <xano.sweeting@gmail.com>
2019-11-24 00:01:19 +0000

    a fuck ton

--- Chat Bot/ChatBot.java
+++ Chat Bot/ChatBot.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
 public class ChatBot {
-
-	public int age;
-	public String name = "Dobby";
-	public String location;
-	public String color; 
-	public String hobby;
-	public String username;
+  // Info about the bot (never changed)
+  private String bot_name = "Xano";
+  private String bot_origin = "New York";
+	private int bot_age = 16;
+	
+  // Info about the person chatting with the bot
+  // (updated once we learn their name)
+	public String user_name = "Mystery person;
 
   // Constructor
   public ChatBot(String n, String l) {
@@ -15,6 +16,7 @@ public class ChatBot {
     location = l;
   }
 
+  /* **************************************************** */
   // Response Generating Helpers
   private String greeting() {
     return "How may I be of assitance master?";
@@ -28,11 +30,44 @@ public class ChatBot {
     return "farewell master " + getUsername();
   }
 
+  /* **************************************************** */
   // Message Parsing Helpers
   private Boolean isQuestion(String message) {
     return message.contains("?");
   }
 
+  // given a string and a list of substring patterns
+  // look for the first occurence of each pattern and return the index of the first match
+  // if none are found, return null
+  private Optional<int> findOperator(String message, String ... patterns) {
+    for (String pattern: patterns) {
+      int idx = message.indexOf(pattern);
+      if (idx > 0) {return idx;}
+    }
+    return null;
+  }
+
+  // return the number in the word before op_idx in message
+  private int parseNumBefore(String message, int op_idx) {
+    // 1. get position of last space before op_idx 
+    // 2. get the substring between last_space_idx and op_idx
+    // 3. return parseInt(last word)
+    int last_space_idx = message.lastIndexOf(" ", op_idx);
+    String number_str = message.substring(last_space_idx, op_idx);
+    return Integer.parseInt(number_str);
+  }
+
+  // return the number in the word after op_idx
+  private int parseNumAfter(String message, int op_idx) {
+    // 1. Find first space after op_indx
+    // 2. get substring of op_idx to first space after it
+    // 3. get integer of substring
+    int first_space_idx = message.indexOf(" ", op_idx);
+    String number_str = message.substring(op_idx, first_space_idx);
+    return Integer.parseInt(number_str);
+  }
+
+  /* **************************************************** */
   // Handlers for the different types of messages
   private String handleHelloGreeting(String message) {
     if (message.contains("Hello") || message.contains("Hi") && !isQuestion(message)) {
@@ -44,7 +79,7 @@ public class ChatBot {
 
   private String howQuestionHandler(String message) {
     if (message.contains("old") && message.contains("you")) {
-      return "I was created at the start of this century";
+      return "I was created in 2003 ... so 16.";
     }
   }
 
@@ -75,76 +110,116 @@ public class ChatBot {
   }
 
   private String handleMathQuestion(String message) {
-    String operator = parseMathOperator(message);
-    Integer firstNum = parseMathNumber(message);
-    Integer secondNum = parseMathNumber(message);
-    Integer answer = null;
+    int op_idx;
 
-    if (message.contains(" plus ") || message.contains(" + ")) {
-      Integer operand1 = message.
-      return "+";
+    // handle addition
+    op_idx = findOperator(message, " plus ", " + ");
+    if (op_idx) {
+      answer = parseNumBefore(message, op_idx) + parseNumAfter(message, op_idx);
+    };
+
+    // handle subtraction
+    op_idx = findOperator(message, " minus ", " - ");
+    if (op_idx) {
+      answer = parseNumBefore(message, op_idx) - parseNumAfter(message, op_ix);
     }
-    else if (message.contains(" minus ") || message.contains(" - ")) {
-      return "-";
+
+    // handle multiplication
+    op_idx = findOperator(message, " times ", " multiplied by ", " * ", " x ");
+    if (op_idx) {
+      answer = parseNumBefore(message, op_idx) * parseNumAfter(message, op_ix);
     }
-    else if (message.contains("times") || message.contains("multiplied by") || message.contains(" x ") || message.contains(" x ")) {
-    }
-    else if (message.contains("divided by") || message.contains("/")) {
+
+    // handle division
+    op_idx = findOperator(message, " divided by ", " / ");
+    if (op_idx) {
+      answer = parseNumBefore(message, op_idx) / parseNumAfter(message, op_idx);
     }
     
+    // if one of the math expression parsers succeeded and we have an answer
+    // return the correctly formatted answer response
     if (answer != null) {
       return "It is " + answer + ".";
-    } else {
-      return null;
     }
+
+    // otherwise return null response so we continue down the list in parse()
+    // and try the next possible handler
+    return null;
   }
 
+  private String parseWhatQuestion(String message) {
+    // handle random question with suggestion to google it
+    if (message.startswith("What ")) {
+      return "Say 'what?' again motherfucker, I dare you!"
+    }
+    return null;
+  }
+
+  private String parseUnknownMessage(String message) {
+    // handle unknown mesage by returning random funny response
+    
+    String[] random_funny_messages = {
+      "There’s a passage I got memorized.",
+      "The path of the righteous man is beset on all sides by the inequities of the selfish and the tyranny of evil men.",
+      "He is truly his brother’s keeper and the finder of lost children.",
+      "But that shit ain’t the truth.",
+      "English motherfucker, do you speak it?",
+    };
+
+    java.util.Random random = new java.util.Random();
+    int random_idx = random.nextInt(random_funny_messages.length);
+    return random_funny_messages[random_idx]);
+
+    // return "There’s a passage I got memorized. Ezekiel 25:17. “The path of the righteous man is beset on all sides by the inequities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of the darkness, for he is truly his brother’s keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who attempt to poison and destroy My brothers. And you will know I am the Lord when I lay My vengeance upon you.” Now… I been sayin’ that shit for years. And if you ever heard it, that meant your ass. You’d be dead right now. I never gave much thought to what it meant. I just thought it was a cold-blooded thing to say to a motherfucker before I popped a cap in his ass. But I saw some shit this mornin’ made me think twice. See, now I’m thinking: maybe it means you’re the evil man. And I’m the righteous man. And Mr. 9mm here… he’s the shepherd protecting my righteous ass in the valley of darkness. Or it could mean you’re the righteous man and I’m the shepherd and it’s the world that’s evil and selfish. And I’d like that. But that shit ain’t the truth. The truth is you’re the weak. And I’m the tyranny of evil men. But I’m tryin’," + user_name + ". I’m tryin’ real hard to be the shepherd."
+  }
+
+  /* **************************************************** */
   // higher level method that parses the user's message
   // and returns a response using the first handler that matches
   public String parse(String message) {
     Optional<String> response;
 
-    // try all of the various message handlers in order
+    // Try all of the various message handlers in order
 
-    response = handleHelloGreeting(message);
+    // Hello, my name is <username>.                -> "Hello <username>. Nice to meet you"
+    response = parseUserNameStatement(message);
     if (response) {return response;}
 
-    
-
-    // What is your name? / Can I have your name please?
-    response = parseNameQuestion(message);
+    // What is your name?                           -> <name>
+    // Can I have your name please?                 -> <name>
+    response = parseBotNameQuestion(message);
     if (response) {return response};
 
-    // How old are you?
-    response = parseWhatAgeQuestion(message);
+    // How old are you?                             -> <age>
+    response = parseBotAgeQuestion(message);
     if (response) {return response};
 
-    // Where are you from? / Are you from ____?
-    response = parseWhereFromQuestion(message);
+    // Where are you from?                          -> <location>
+    // Are you from <location>?                     -> Yes/No
+    response = parseBotOriginQuestion(message);
     if (response) {return response};
 
-    // What is <num> <op> <num>
-    response = handleMathQuestion(message);
+    // What is <num> <op> <num>?                    -> <answer>
+    // What is the answer of <num> <op> <num>?      -> <answer>
+    // Can you tell me what <num> <op> <num> is?    -> <answer>
+    response = parseMathQuestion(message);
     if (response) {return mathResponse;}
 
-    // What is ____?
-    response = parseWhatIsQuestion(message);
+    // What is ____?                                -> "Not sure..."
+    response = parseWhatQuestion(message);
     if (response) {return response};
 
-    // if none, succeed, attempt to distract them instead
-    return handleUknownQuestion(message);
+    // Uknown message                               -> random funny thing
+    return parseUnknownMessage(message);
   }
 
+  /* **************************************************** */
   public static void main(String[] args) {
 	  ChatBot bot = new ChatBot();
     Scanner scanner = new Scanner(System.in);
 
-    // Print initial greeting message
-    System.out.println(
-      "Welcome to the chatbot program, type " +
-      "'Hello, my name is abcdef.'" + 
-      " to get started!"
-    );
+    // Print initial greeting message (is this needed?)
+    // System.out.println("Welcome to the chatbot program, type 'Hello, my name is abcdef.' to get started!");
 
     while (true) {
       // 1. Get message from user
